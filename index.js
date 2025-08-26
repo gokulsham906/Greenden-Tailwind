2d54822686bcfe2695792eb50be5577fe9b01625
gokulsham906 <gokulsham906@gmail.com>
2025-08-26 21:56:49 +0530

    adding js files

---
+++ index.js
@@ -0,0 +1,12 @@
+const sidenav = document.getElementById("sidenav");
+const menuicon = document.getElementById("menuicon");
+const closenav = document.getElementById("close-nav");
+
+
+menuicon.addEventListener("click", function () {
+  sidenav.style.right = "0%";
+});
+
+closenav.addEventListener("click", function () {
+  sidenav.style.right = "-100%";
+});
